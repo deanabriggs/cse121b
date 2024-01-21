f46b07498a74af0c0e5c35f86103c53ccefc7d7d
deanabriggs <deanambriggs@gmail.com>
2024-01-21 00:53:58 -0800

    Completed Assigned w2

--- w2/scripts/w02-task.js
+++ w2/scripts/w02-task.js
@@ -4,32 +4,40 @@
 
 /* Step 2 - Variables */
 const fullName = "Deana Briggs";
-
 let currentDate = new Date();
 let currentYear = currentDate.getFullYear();
-
 const profilePicture = "images/myphoto.jpg";
-
 
 /* Step 3 - Element Variables */
 
+let nameElement = document.querySelector('#name');
 let foodElement = document.getElementById('food');
 let yearElement = document.querySelector('#year');
-let imageElement = document.querySelectorAll('img');
-
+const imageElement = document.querySelector('img');
 
 /* Step 4 - Adding Content */
 
-let nameElement = document.querySelector('#name');
 nameElement.innerHTML = `<strong>${fullName}</strong>`;
-
-
 yearElement.textContent = currentYear;
-imageElement.setAttribute = ('src', profilePicture);
-imageElement.setAttribute = ('alt', `Profile image of ${fullName}`)
-
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
+
+let favFood = ['pasta', 'yogurt parfait', 'sandwiches', 'ice cream', 'roast', 'fruit'];
+foodElement.innerHTML = favFood;
+
+let anotherFav = 'popcorn';
+favFood.push(anotherFav);
+foodElement.innerHTML += `<br>${favFood}`;
+
+favFood.shift();
+foodElement.innerHTML += `<br>${favFood}`;
+
+favFood.pop();
+foodElement.innerHTML += `<br>${favFood}`;
+
+
 
 
 
