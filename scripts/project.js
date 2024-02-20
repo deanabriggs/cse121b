e0fa89789f32110b6f84f189a77fd6f0b73f4973
deanabriggs <deanambriggs@gmail.com>
2024-02-19 23:43:03 -0800

    Project - Phase 1 (has duplication issus)

---
+++ scripts/project.js
@@ -0,0 +1,132 @@
+/* Global Variables */
+const myApiNinjaKey = "B6Fj6jjEGtwbShQAek5TtQ==2EVLcZjpyptQs0w3";
+const dogDetail = [];
+const breedsNameData = [];
+const dogNameList = [];
+
+const displayDogs = async () => {
+    try {
+        await getDogDetails();
+
+        const dogsElement = document.querySelector('#dogs');
+
+        dogDetail.sort((a, b) => {
+            const dogNameA = Object.keys(a)[0];
+            const dogNameB = Object.keys(b)[0];
+            return dogNameA.localeCompare(dogNameB);
+        })
+        console.log(dogDetail);
+
+        dogDetail.forEach((dogInfo) => {
+            let dogName = Object.keys(dogInfo)[0];
+            let dt = dogInfo[dogName];
+
+            console.log(dt);
+
+            let article = document.createElement("article");
+            let h3 = document.createElement("h3");
+            h3.textContent = `${dogName}`
+            let img = document.createElement("img");
+            img.src = `${dt.image_link}`;
+            img.alt = `Picture of ${dogName}`;
+            
+            let p1 = document.createElement("p");
+            let life = `Life: ${dt.min_life_expectancy}-${dt.max_life_expectancy}`;
+            let weight = `Weight: ${dt.min_weight_female}-${dt.max_weight_male}`;
+            let height = `Height: ${dt.min_height_female}-${dt.max_height_male}`;
+            p1.textContent = life + ` | ` + weight + ` | ` + height;
+
+            let p2 = document.createElement("p");
+            let shed = `Shed: ${dt.shedding}`;
+            let coat = `Coat Lgth: ${dt.coat_length}`;
+            let groom = `Groom: ${dt.grooming}`;
+            let drool  = `Drool: ${dt.drooling}`;
+            p2.textContent = shed + ` | ` + coat + ` | ` + groom + ` | ` + drool;
+
+            let p3 = document.createElement("p");
+            let kids = `Kids: ${dt.good_with_children}`;
+            let dogs = `Other Dogs: ${dt.good_with_other_dogs}`;
+            let strangers = `Stangers: ${dt.good_with_strangers}`;
+            let protect = `Protect: ${dt.protectiveness}`;
+            p3.textContent = kids + ` | ` + dogs + ` | ` + strangers + ` | ` + protect;
+            
+            let p4 = document.createElement("p");
+            let play = `Playful: ${dt.playfulness}`;
+            let train = `Trainable: ${dt.trainability}`;
+            let energy = `Energy: ${dt.energy}`;
+            let bark = `Bark: ${dt.barking}`;
+            p4.textContent = play + ` | ` + train + ` | ` + energy + ` | ` + bark;
+            
+            article.appendChild(h3);
+            article.appendChild(img);
+            article.appendChild(p1);
+            article.appendChild(p2);
+            article.appendChild(p3);
+            article.appendChild(p4);
+            dogsElement.appendChild(article);
+        });
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+};
+
+const getBreeds = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/list/all");
+    if (!response.ok) {
+        throw new Error(`Failed to fetch breeds: ${response.status}`);
+    }
+    const data = await response.json();
+
+    for (const breedName in data.message) {
+        const breedTypes = data.message[breedName];
+
+        if(!dogNameList.includes(breedName)) {
+            dogNameList.push(breedName.trim());
+        }
+
+        if (breedTypes.length !== 0) {
+            breedTypes.forEach(subBreed => {
+                let fullName = `${subBreed.trim() + " " + breedName.trim()}`;
+                if (!dogNameList.includes(fullName)) {
+                    dogNameList.push(fullName);
+                }
+            });
+        }
+    }
+    console.log(dogNameList);
+};
+
+const getDogDetails = async () => {
+    try {
+        await getBreeds();
+        await Promise.all(dogNameList.map(async (dogName) => {
+            const response = await fetch('https://api.api-ninjas.com/v1/dogs?limit=1&name=' + dogName, {
+                headers: {'X-Api-Key': myApiNinjaKey}
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const dogInfo = await response.json();
+            if (dogInfo && dogInfo.length > 0) {             
+                const {name, ...traits} = dogInfo[0];
+                const newDog = {[name]: traits};
+                dogDetail.push(newDog);
+            } 
+        }));
+
+    } catch (error) {
+        console.error("Error:", error.message);
+    }
+}
+
+function reset(){
+    document.querySelector("#dogs").innerHTML = "";
+}
+
+function filterDogs(dogs) {
+    reset();
+    let filter = document.get
+}
+
+displayDogs();
+
